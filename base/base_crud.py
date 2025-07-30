121c39e7f2a5a4329922f18f47be289e1e1e6644
IvanSteksov <stecsov.vanys@mail.ru>
2025-07-30 07:15:44 +0300

    changed base crud

--- base/base_crud.py
+++ base/base_crud.py
@@ -1,7 +1,7 @@
 from typing import Any, Optional
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy import delete, select, update
+from sqlalchemy import delete, select, update, exists, and_
 
 
 class CRUD:
@@ -16,19 +16,42 @@ class CRUD:
         await self.session.refresh(new)
         return new
 
+    async def read_all(self):
+        stmt = select(self.table)
+        result = await self.session.execute(stmt)
+        records = result.scalars().all()
+        return records
+
     async def delete_by_filter(self, filt: Any) -> int:
-        stmt = delete(self.table).where(*filt)
+        condition = filt if not isinstance(filt, (list, tuple)) else and_(*filt)
+        stmt = delete(self.table).where(condition)
         result = await self.session.execute(stmt)
         await self.session.commit()
         return result.rowcount
 
     async def read_by_filter(self, filt: Any) -> Optional[list]:
-        stmt = select(self.table).where(*filt)
+        condition = filt if not isinstance(filt, (list, tuple)) else and_(*filt)
+        stmt = select(self.table).where(condition)
         result = await self.session.execute(stmt)
         return result.scalars().all()
 
+    async def get(self, id: int) -> DeclarativeBase:
+        return await self.session.get(self.table, id)
+
+    async def delete(self, obj):
+        await self.session.delete(obj)
+        await self.session.commit()
+
     async def update_by_filter(self, new_vals: dict, filt: Any) -> int:
-        stmt = update(self.table).where(*filt).values(**new_vals)
+        condition = filt if not isinstance(filt, (list, tuple)) else and_(*filt)
+        stmt = update(self.table).where(condition).values(**new_vals)
         result = await self.session.execute(stmt)
         await self.session.commit()
         return result.rowcount
+
+    async def check_by_filter(self, filt: Any) -> bool:
+        condition = filt if not isinstance(filt, (list, tuple)) else and_(*filt)
+        stmt = select(exists().where(condition)).select_from(self.table)
+        result = await self.session.execute(stmt)
+        return result.scalars().one()
+
